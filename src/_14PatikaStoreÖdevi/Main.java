6e91f991e1be30dfb46aaf8cd4a3ce688aea6a7e
muhammetali.karaguzel61@gmail.com <muhammetali.karaguzel61@gmail.com>
2023-05-27 22:22:21 +0300

    Fikstür Oluşturma Ödevi

---
+++ src/_14PatikaStoreÖdevi/Main.java
@@ -0,0 +1,85 @@
+package _14PatikaStoreÖdevi;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> teams = new ArrayList<>();
+        addTeam(teams);
+        addBayTeam(teams);
+        ArrayList<String> matchlist = match(teams);
+        printMatch(matchlist);
+
+    }
+
+
+    //Burada kullanıcı istediği kadar takım ekleyebilir. Eklemeyi bitirdikten sonra ise 0 veya exit yazarak ekleme işlemini sonlandırabilir.
+    public static void addTeam(ArrayList arrayList){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lütfen eklemek istediğiniz takımın adını giriniz.");
+        System.out.println("Takim eklemeyi bitirmek istediğinizde '0' veya 'exit' giriniz!!!");
+        String team;
+        while (true){
+            team = sc.nextLine();
+            if(team.equals("0") || team.equals("exit")){
+                break;
+            }
+            arrayList.add(team);
+        }
+    }
+
+    //Eklenen takım sayısı tek ise "bay" adında bir takım ekliyoruz
+    public static void addBayTeam(ArrayList arrayList){
+        if(arrayList.size() %2 == 1){
+            arrayList.add("Bay");
+        }
+    }
+
+    public static void arrayPrint(ArrayList<String> arrayList){
+        for (String str : arrayList){
+            System.out.println(str);
+        }
+    }
+
+    public static ArrayList<String> match(ArrayList<String> array) {
+        ArrayList matchList = new ArrayList<>();
+        Collections.shuffle(array); // Burada bir nevi random işi yaparak takımları rastgele eşleştiriyoruz.
+        String str = "";
+        String str2 = "";
+        int size = array.size();
+        for (int away = size / 2; away < size; away++) { // 4-5-6-7 takımlar away olarak oynar ligin ilk yarısını.(8 elemanlı bir lig için)
+            for (int home = 0; home < size / 2; home++) { //0-1-2-3 takımlar home olarak oynar ligin ilk yarısını.(8 elemanlı bir lig için)
+                if (home + away >= size) {
+                    int ha = (home + away) % (size / 2);
+                    str += array.get(home) + " vs " + array.get(ha) + "\n";
+                    str2 += array.get(ha) + " vs " + array.get(home) + "\n";
+                } else {
+                    str += array.get(home) + " vs " + array.get(home + away) + "\n";
+                    str2 += array.get(home + away) + " vs " + array.get(home) + "\n";
+                }
+            }
+            matchList.add(str);
+            matchList.add(str2);
+            str = "";
+            str2 = "";
+        }
+
+        return matchList;
+    }
+
+    //MatchListe ligin birinci yarısı tek sayılar 2. yarısı çift sayılar olarak tutuluyor.
+    public static void printMatch(ArrayList<String> array){
+        for(int i = 0; i<array.size(); i+=2){
+            System.out.println(array.get(i));
+            System.out.println("=================");
+        }
+        for(int i = 1; i<array.size(); i+=2){
+            System.out.println(array.get(i));
+            System.out.println("=================");
+        }
+    }
+
+}
+
---
+++ src/_14PatikaStoreÖdevi/ReadMe.md
@@ -0,0 +1,21 @@
+<h2>Fikstür Oluşturucu</h2>
+
+<p>Java ile girilen takımlar için rastgele maç fikstürü oluşturan bir sınıf yazılmalı.</p>
+<p>Kurallar :</p>
+<ul>
+<li>Çift Devreli Lig usülü uygulanacaktır. Her takım diğer takımlarla kendi evinde ve deplasmanda olmak üzere iki maç yapacaktır.</li>
+<li>Listenin sol tarafı ev sahibini sağ tarafı deplasman takımını gösterir.</li>
+<li>Eğer tek sayıda takım listesi girilirse, çift sayıya tamamlanacak şekilde "Bay" adında bir takım daha eklenmeli. Bay ile eşleşen takımlar o hafta maç yapmayacağı anlamına gelir.</li>
+</ul>
+
+<h2>Takım sayısı çift senaryosu</h2>
+
+<img src="./images/Çift1.png">
+<img src="./images/Çift2.png">
+
+
+<h2>Takım sayısı tek senaryosu</h2>
+
+<img src="./images/Tek1.png">
+<img src="./images/Tek2.png">
+<img src="./images/Tek3.png">

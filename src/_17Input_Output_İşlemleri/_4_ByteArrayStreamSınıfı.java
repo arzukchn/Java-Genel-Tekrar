eb94b8faff9b548fecf20222b19b9d7290788c3b
muhammetali.karaguzel61@gmail.com <muhammetali.karaguzel61@gmail.com>
2023-06-16 17:34:16 +0300

    ByteArrayInputSınıfı

---
+++ src/_17Input_Output_İşlemleri/_4_ByteArrayStreamSınıfı.java
@@ -0,0 +1,25 @@
+package _17Input_Output_İşlemleri;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class _4_ByteArrayStreamSınıfı {
+    public static void main(String[] args) {
+        byte[] dizi = {1,2,3,4,5,6};
+        //Alttaki kısımda offset ile dizinin nerden başlanacağı, length ile de başladıktan sonra kaç adet eleman ile devam edeceği seçilir.
+        //Bizim dizimize göre 0 indexinden yani 1 den başlayıp 4de bitecektir.
+        ByteArrayInputStream input = new ByteArrayInputStream(dizi , 0, 4);
+        System.out.println("Kullanılabilen byte sayisi : " + input.available());
+        //input.skip(2); --> Atlamak amacıyla kullanılır.
+        int i = input.read();
+        while (i != -1){
+            System.out.println(i);
+            i = input.read();
+        }
+        try {
+            input.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
